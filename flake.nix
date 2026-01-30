6cb040116a8ba4172e2de3677a6a8105e81ca831
Giovanni d'Amelio <gio@damelio.net>
2026-01-30 13:04:30 -0800

    Add a simple devshell with a few packaging related deps

--- flake.nix
+++ flake.nix
@@ -5,5 +5,23 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {};
+  outputs = { self, nixpkgs }: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nix-init # Help scaffold new package automatically
+          nurl # Fetch a URL and give a Nix style fetch statement with the correct hash
+          nix-update # Easily Auto Update version/src hashs for derivations
+        ];
+      };
+    });
+  };
 }
