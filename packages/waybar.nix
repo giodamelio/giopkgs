66f3ebcb508ab2857f01e1955bb7a93379ba5173
Giovanni d'Amelio <gio@damelio.net>
2026-02-07 15:00:39 -0800

    Add a patched version of waybar

---
+++ packages/waybar.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+pkgs.waybar.overrideAttrs (oldAttrs: {
+  patches = (oldAttrs.patches or []) ++ [
+    # Adds cpu_graph module with line, gauge, and stacked bar graph types
+    (pkgs.fetchurl {
+      url = "https://patch-diff.githubusercontent.com/raw/Alexays/Waybar/pull/4588.patch";
+      hash = "sha256-t+72Wam1GsgG3Ra2ZVDIInV/g0PL2ddpvdxyXnn6//8=";
+    })
+  ];
+
+  meta = oldAttrs.meta // {
+    description = "Waybar with PR #4588 (additional features/fixes)";
+  };
+})
