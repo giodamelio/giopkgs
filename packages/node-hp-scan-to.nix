42eaf9976a1a711dc764ffa83e86d6f28b64d686
Giovanni d'Amelio <gio@damelio.net>
2026-03-23 09:31:28 -0700

    Package a newer version of node-hp-scan-to

---
+++ packages/node-hp-scan-to.nix
@@ -0,0 +1,52 @@
+{
+  stdenv,
+  fetchFromGitHub,
+  makeWrapper,
+  nodejs,
+  yarn-berry,
+  node-hp-scan-to,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "node-hp-scan-to";
+  version = "1.9.7";
+
+  src = fetchFromGitHub {
+    owner = "manuc66";
+    repo = "node-hp-scan-to";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-+9cB/hLw426uKzJd6n0aWLtAHhri+DYWqeuZt88QwRo=";
+  };
+
+  missingHashes = ./node-hp-scan-to-missing-hashes.json;
+
+  offlineCache = yarn-berry.fetchYarnBerryDeps {
+    inherit (finalAttrs) src missingHashes;
+    hash = "sha256-GPdd40tYYViGXSFsZQ73yxy4JgUKqLFWMv0N2blFKEo=";
+  };
+
+  nativeBuildInputs = [
+    makeWrapper
+    nodejs
+    yarn-berry
+    yarn-berry.yarnBerryConfigHook
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    yarn install --immutable
+    yarn build
+
+    yarn workspaces focus --production
+
+    mkdir -p "$out/lib/node_modules/node-hp-scan-to"
+    cp -r dist node_modules package.json "$out/lib/node_modules/node-hp-scan-to"
+
+    makeWrapper "${nodejs}/bin/node" "$out/bin/node-hp-scan-to" \
+      --add-flags "$out/lib/node_modules/node-hp-scan-to/dist/index.js"
+
+    runHook postInstall
+  '';
+
+  inherit (node-hp-scan-to) meta;
+})
