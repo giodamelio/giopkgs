5b2028d21b08d29735fe2c86ac5b00376b48bc6f
Giovanni d'Amelio <gio@damelio.net>
2026-04-21 15:44:37 -0700

    Add a patched version of wezterm

---
+++ packages/wezterm.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  fetchpatch,
+  ...
+}:
+pkgs.wezterm.overrideAttrs (oldAttrs: {
+  patches =
+    (oldAttrs.patches or [])
+    ++ [
+      # This PR adds a new pane-focus-changed I want
+      # REMIND-ME-TO: Remove this patch pr_released=github:wezterm/wezterm#7510
+      (fetchpatch {
+        url = "https://patch-diff.githubusercontent.com/raw/wezterm/wezterm/pull/7510.patch";
+        hash = "sha256-iIwkg1Tf/tcZQlCqsPeu32uu6fKV0ye9JaKlU6skF00=";
+      })
+    ];
+
+  meta =
+    oldAttrs.meta
+    // {
+      description = "Wezterm with PR #7510";
+    };
+})
