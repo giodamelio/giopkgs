92b346982bf3d27a9f642cdab1a60a805e1da3f9
Giovanni d'Amelio <gio@damelio.net>
2026-04-21 20:04:22 -0700

    Update remind-me-to

--- packages/remind-me-to/package.nix
+++ packages/remind-me-to/package.nix
@@ -5,13 +5,13 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "remind-me-to";
-  version = "0-unstable-2026-04-21";
+  version = "0-unstable-2026-04-22";
 
   src = fetchFromGitHub {
     owner = "giodamelio";
     repo = "remind-me-to";
-    rev = "6397dba4d58c2a1a528965d0aeacce4b9e377c36";
-    hash = "sha256-d0m7c0DeTfgFLZXZpJcqqF6F7uzMlz0yRee7aylkExE=";
+    rev = "c5e778d54ccd932301aeca2f7238126bcf9e52b3";
+    hash = "sha256-MYuWuSIbcAB6oaUhlvHQNAatn9ddECxh7hFlAIfBxFs=";
   };
 
   cargoHash = "sha256-eftfZdAuzwggpZPqnVnmiLXQj2r7BiOTygijCmlN13Q=";
