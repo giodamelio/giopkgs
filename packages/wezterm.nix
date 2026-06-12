24aaf11a18d22d7da2caeabccf3f8cd773f3e37b
Giovanni d'Amelio <gio@damelio.net>
2026-06-12 14:50:00 -0700

    Add wezterm PR #7787 patch (fix kitty ESC key)

--- packages/wezterm.nix
+++ packages/wezterm.nix
@@ -20,6 +20,13 @@ pkgs.wezterm.overrideAttrs (oldAttrs: {
         url = "https://github.com/wezterm/wezterm/commit/3f062e0aa1924dc5666a57b0e7d065cc26a3b29b.patch";
         hash = "sha256-wEpl9ODe6evUQCem7BgoavENl+iQtRKHVtmIS5UelNI=";
       })
+
+      # Fix kitty spec for ESC key (needing to hit Esc twice)
+      # REMIND-ME-TO: Remove this patch pr_released=github:wezterm/wezterm#7787
+      (fetchpatch {
+        url = "https://patch-diff.githubusercontent.com/raw/wezterm/wezterm/pull/7787.patch";
+        hash = "sha256-hYhb5foZ3rqGEGNZXmToaNGgP1IOHcih4S5btrJionk=";
+      })
     ];
 
   passthru =
@@ -31,6 +38,6 @@ pkgs.wezterm.overrideAttrs (oldAttrs: {
   meta =
     oldAttrs.meta
     // {
-      description = "Wezterm with PR #7510 and PR #7034";
+      description = "Wezterm with PR #7510, PR #7034 and PR #7787";
     };
 })
