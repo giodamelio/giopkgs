74313a5758ae2835ef9d43517c970f65b7e6a794
Giovanni d'Amelio <gio@damelio.net>
2026-09-25 20:20:03 -0700

    chore(qutebrowser): 3.7.0-unstable-2026-09-25 -> 3.7.0-unstable-2026-09-26

--- packages/qutebrowser/package.nix
+++ packages/qutebrowser/package.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}:
 pkgs.qutebrowser.overrideAttrs (oldAttrs: {
-  version = "3.7.0-unstable-2026-09-25";
+  version = "3.7.0-unstable-2026-09-26";
 
   src = pkgs.fetchFromGitHub {
     owner = "giodamelio";
     repo = "qutebrowser";
-    rev = "4d5c4a83d5a14886180e7bccdc042e2e61ee509a";
-    hash = "sha256-DCjEjNFWKBDH9pKgXxcyKWmULZTCHMk4P3s2qsxTSGM=";
+    rev = "e8658c415915a247d2ec31d8c48c4a99a3e27512";
+    hash = "sha256-OTNMy0NOwwU3WnZTkZvdJsRS94EsWaRDTCEEiSUOtCM=";
   };
 
   passthru =
